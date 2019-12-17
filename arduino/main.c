b8fca45c2c876abf1ed61caddbe46af8730dd0fa
20181102931 <18647197975@163.com>
2019-12-17 15:19:40 +0800

    Initial Commit

---
+++ arduino/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  arduino
+//
+//  Created by s20181102931 on 2019/12/17.
+//  Copyright © 2019 s20181102931. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
